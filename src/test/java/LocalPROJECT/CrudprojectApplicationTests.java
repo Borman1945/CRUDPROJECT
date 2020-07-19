3d7187b890a7f83b002f25d1629865d873e290ec
MoshkaBortmam <TryagenButor.ru>
2020-07-19 20:11:33 +0300

    Initial commit

---
+++ src/test/java/LocalPROJECT/CrudprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package LocalPROJECT;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

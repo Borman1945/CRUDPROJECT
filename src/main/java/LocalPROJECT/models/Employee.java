c9d046d2b4a289d7a5c17a07ce8fe1796767388d
MoshkaBortmam <TryagenButor.ru>
2020-07-19 20:23:51 +0300

    new dependency lambok and delet getter, setters from models

--- src/main/java/LocalPROJECT/models/Employee.java
+++ src/main/java/LocalPROJECT/models/Employee.java
@@ -1,5 +1,8 @@
 package LocalPROJECT.models;
 
+import lombok.Getter;
+import lombok.Setter;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -9,6 +12,8 @@ import javax.persistence.Table;
 
 @Entity
 @Table(name = "employees")
+@Getter
+@Setter
 public class Employee {
 
     @Id
@@ -29,44 +34,6 @@ public class Employee {
         this.firstName = firstName;
         this.lastName = lastName;
         this.emailId = emailId;
-    }
-
-
-    public long getId() {
-        return id;
-    }
-    public void setId(long id) {
-        this.id = id;
-    }
-
-
-    public String getFirstName() {
-        return firstName;
-    }
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
-
-    public String getLastName() {
-        return lastName;
-    }
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
-
-
-    public String getEmailId() {
-        return emailId;
-    }
-    public void setEmailId(String emailId) {
-        this.emailId = emailId;
-    }
-
-    @Override
-    public String toString() {
-        return "Employee [id=" + id + ", firstName=" + firstName + ", lastName=" + lastName + ", emailId=" + emailId
-                + "]";
     }
 
 }
